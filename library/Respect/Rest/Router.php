eaa914db9f31a8f66fc6bb29225ebcd57c510d79
Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
2011-04-12 20:51:11 -0300

    Initial commit. README, Licensing, etc. Router and tests.

---
+++ library/Respect/Rest/Router.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Respect\Rest;
+
+class Router
+{
+
+    protected $routes = array();
+
+    public function addRoute($httpMethod, $path, $callback)
+    {
+        $httpMethod = strtoupper($httpMethod);
+        $pathRegex = $this->compileRouteRegex($path);
+        $this->routes[$httpMethod][$pathRegex] = $callback;
+        uksort($this->routes[$httpMethod],
+            function($a, $b) {
+                return substr_count($a, '.+?') < substr_count($b, '.+?');
+            }
+        );
+    }
+
+    protected function compileRouteRegex($path)
+    {
+        $path = rtrim($path, ' /');
+        $extra = $this->extractCatchAllPattern($path);
+        $pathQuoted = str_replace('/\*', '/(.+?)', preg_quote($path));
+        $pathQuoted = $this->compileOptionalParams($pathQuoted);
+        $pathRegex = "#^{$pathQuoted}{$extra}$#";
+        return $pathRegex;
+    }
+
+    protected function extractCatchAllPattern(&$path)
+    {
+        $extra = '(/.*)?';
+
+        if (strlen($path) - 2 === strripos($path, '/**'))
+            $path = substr($path, 0, -3);
+        else
+            $extra = '';
+
+        $path = str_replace('**', '*', $path);
+        return $extra;
+    }
+
+    //turn sequenced parameters optional, so /*/*/* will match /1/2/3, /1/2 and /1
+    protected function compileOptionalParams($pathQuoted)
+    {
+        while (strlen($pathQuoted) - 12 === strripos($pathQuoted, '/(.+?)/(.+?)'))
+            $pathQuoted = str_replace(
+                    array('/(.+?)/(.+?)', '(?:/(.+?))?/(.+?)'),
+                    array('/(.+?)(?:/(.+?))?', '(?:/(.+?))?(?:/(.+?))?'),
+                    $pathQuoted
+            );
+        return $pathQuoted;
+    }
+
+    public function dispatch($httpMethod=null, $httpUri=null)
+    {
+        $httpMethod = strtoupper($httpMethod ? : $_SERVER['REQUEST_METHOD']);
+        $httpUri = $httpUri ? : parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $httpUri = rtrim($httpUri, ' /');
+        if ($this->routes[$httpMethod])
+            foreach ($this->routes[$httpMethod] as $pathRegex => $callback)
+                if (preg_match($pathRegex, $httpUri, $params))
+                    return call_user_func_array(
+                        $callback, array_filter(array_slice($params, 1))
+                    );
+    }
+
+}
+
+/**
+ * LICENSE
+ *
+ * Copyright (c) 2009-2011, Alexandre Gomes Gaigalas.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of Alexandre Gomes Gaigalas nor the names of its
+ *       contributors may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
---
+++ tests/library/Respect/Rest/RouterTest.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Respect\Rest;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $object;
+    protected $result;
+    protected $callback;
+
+    public function setUp()
+    {
+        $this->object = new Router;
+        $this->result = null;
+        $result = &$this->result;
+        $this->callback = function() use(&$result) {
+                $result = func_get_args();
+            };
+    }
+
+    /**
+     * @dataProvider providerForSingleRoutes
+     */
+    public function testSingleRoutes($route, $path, $expectedParams)
+    {
+        $this->object->addRoute('get', $route, $this->callback);
+        $this->object->dispatch('get', $path);
+        $this->assertEquals($expectedParams, $this->result);
+    }
+
+    /**
+     * @dataProvider providerForLargeParams
+     */
+    public function testLargeParams($route, $path, $expectedParams)
+    {
+
+        $this->object->addRoute('get', $route, $this->callback);
+        $this->object->dispatch('get', $path);
+        $this->assertEquals($expectedParams, $this->result);
+    }
+
+    /**
+     * @dataProvider providerForSpecialChars
+     */
+    public function testSpecialChars($route, $path, $expectedParams)
+    {
+
+        $this->object->addRoute('get', $route, $this->callback);
+        $this->object->dispatch('get', $path);
+        $this->assertEquals($expectedParams, $this->result);
+    }
+
+    public function providerForSingleRoutes()
+    {
+        return array(
+            array(
+                '/',
+                '/',
+                array()
+            ),
+            array(
+                '/users',
+                '/users',
+                array()
+            ),
+            array(
+                '/users/',
+                '/users',
+                array()
+            ),
+            array(
+                '/users',
+                '/users/',
+                array()
+            ),
+            array(
+                '/users/*',
+                '/users/1',
+                array(1)
+            ),
+            array(
+                '/users/*/*',
+                '/users/1/2',
+                array(1, 2)
+            ),
+            array(
+                '/users/*/lists',
+                '/users/1/lists',
+                array(1)
+            ),
+            array(
+                '/users/*/lists/*',
+                '/users/1/lists/2',
+                array(1, 2)
+            ),
+            array(
+                '/users/*/lists/*/*',
+                '/users/1/lists/2/3',
+                array(1, 2, 3)
+            ),
+            array(
+                '/users/*/*/lists/*/*',
+                '/users/1/1B/lists/2/3',
+                array(1, '1B', 2, 3)
+            ),
+            array(
+                '/users/*/mounted-folder/**',
+                '/users/alganet/mounted-folder/home/alganet/Projects/RespectRest/',
+                array('alganet', 'home/alganet/Projects/RespectRest')
+            ),
+        );
+    }
+
+    public function providerForLargeParams()
+    {
+        return
+        array(
+            array(
+                '/users/*/*/*/*/*/*/*',
+                '/users/1',
+                array(1)
+            ),
+            array(
+                '/users/*/*/*/*/*/*/*',
+                '/users/a/a/a/a/a/a/a',
+                array('a', 'a', 'a', 'a', 'a', 'a', 'a')
+            ),
+            array(
+                '/users' . str_repeat('/*', 300), //300 short parameters
+                '/users' . str_repeat('/xy', 300),
+                str_split(str_repeat('xy', 300), 2)
+            ),
+            array(
+                '/users' . str_repeat('/*', 60), //60 large parameters
+                '/users' . str_repeat('/abcdefghijklmnopqrstuvwxyz', 60),
+                str_split(str_repeat('abcdefghijklmnopqrstuvwxyz', 60), 26)
+            ),
+        );
+    }
+
+    public function providerForSpecialChars()
+    {
+        return
+        array(
+            array(
+                '/My Documents/*',
+                '/My Documents/1',
+                array(1)
+            ),
+            array(
+                '/My%20Documents/*', //trival
+                '/My%20Documents/1',
+                array(1)
+            ),
+            array(
+                '/(.*)/*/[a-z]/*', //preg_quote ftw, but you're a SOB if you
+                '/(.*)/1/[a-z]/2', //create a route with those special chars
+                array(1, 2)
+            ),
+        );
+    }
+
+}
+
