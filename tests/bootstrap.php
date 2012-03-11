479aeb05d9f642de86c83e744e5792d4cd6096fe
Augusto Pascutti <augusto@phpsp.org.br>
2012-03-11 01:20:45 -0300

    Added support for a callback autoloader without Composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,4 +12,21 @@ set_include_path('../library'
  * If this file does not exist, run `composer.phar install` from
  * the project root directory to generate it.
  */
-require realpath(dirname(__FILE__) . '/../vendor/.composer/autoload.php');
+if (file_exists($c=dirname(__FILE__).'/../vendor/.composer/autoload.php'))
+	require realpath($c);
+else 
+	spl_autoload_register(
+	    function($className) {
+	        $fileParts = explode('\\', ltrim($className, '\\'));
+
+	        if (false !== strpos(end($fileParts), '_'))
+	            array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
+
+	        $file = implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
+
+	        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+	            if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file))
+	                return require $path;
+	        }
+	    }
+	);
