Respect/Rest

commit 0155166db39a7b9403e865a6a6c5b0b2ea4f7740

Pass request parameters to AuthBasic routine (as 3rd and following parameters).
Comes handy if the authentication depends on the request parameters. For example, if you were implementing the github repos API with BasicAuthentication, and you want to GET information from a private repository like this `api.github.com/repos/Respect/SecretRepo/commits`, the required username/password would depend on that first parameter:

```php
<?php
$reposRoute->authBasic('Repository API', function ($name, $password, $userParam, $repoParam) {
        $authUser = Users::findByName($name);
        return ($password === $authUser->checkPassword($password)) && ($user->mayAccessRepository($userParam, $repoParam));
};

```

library/Respect/Rest/Routines/AuthBasic.php
@@ -20,10 +20,12 @@ class AuthBasic extends AbstractRoutine implements ProxyableBy
 		if (isset($_SERVER['HTTP_AUTHORIZATION']))
 			return call_user_func_array(
 				$this->callback,
-				explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6))
+				array_merge(explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)), $params)
 			));
 		elseif (isset($_SERVER['PHP_AUTH_USER']))
-			return call_user_func($this->callback, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+                       return call_user_func_array(
+                               $this->callback,
+                               array_merge(array($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']), $params));
 
 		header('HTTP/1.1 401');
 		header("WWW-Authenticate: Basic realm=\"{$this->realm}\"");

commit 9caf04151bc6779c89258dcae30b320e951dd283

add test for AuthBasic routine; fix for HTTP_AUTHORIZATION (broken in 0155166db39a7b9403e865a6a6c5b0b2ea4f7740 )

library/Respect/Rest/Routines/AuthBasic.php
@@ -20,8 +20,8 @@ class AuthBasic extends AbstractRoutine implements ProxyableBy
 		if (isset($_SERVER['HTTP_AUTHORIZATION']))
 			return call_user_func_array(
 				$this->callback,
-				array_merge(explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)), $params)
-			));
+				array_merge(explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6))), $params)
+			);
 		elseif (isset($_SERVER['PHP_AUTH_USER']))
                        return call_user_func_array(
                                $this->callback,

tests/library/Respect/Rest/RouterTest.php
@@ -312,6 +312,29 @@ namespace Respect\Rest {
             unset($_SERVER['PHP_AUTH_PW'], $_SERVER['PHP_AUTH_USER']);
         }
 
+        function test_auth_basic_pass_all_parameters_to_routine()
+        {
+            global $header;
+            $user = 'John';
+            $pass = 'Doe';
+            $param1 = 'parameterX';
+            $param2 = 'parameterY';
+            $checkpoint = false;
+            $_SERVER['PHP_AUTH_USER'] = $user;
+            $_SERVER['PHP_AUTH_PW'] = $pass;
+            $this->router->get('/*/*', 'ok')->authBasic("Test Realm", function($username, $password, $p1, $p2) use (&$checkpoint, $user, $pass, $param1, $param2)
+            {
+                if (($p1 === $param1) && $p2 === $param2) {
+                    $checkpoint = true;
+                    return true;
+                }
+                return false;
+            });
+            (string)$this->router->dispatch('GET', "/$param1/$param2")->response();
+            $this->assertTrue($checkpoint, 'Parameters passed incorrectly');
+            unset($_SERVER['PHP_AUTH_PW'], $_SERVER['PHP_AUTH_USER']);
+        }
+
         /**
          * @group issues
          * @ticket 37

tests/library/Respect/Rest/Routines/AuthBasicTest.php
@@ -1,0 +1,50 @@
+<?php
+
+namespace Respect\Rest\Routines;
+
+/**
+ * @since 21.05.12
+ * @brief new class. write something here, plz
+ */
+class AuthBasicTest extends \PHPUnit_Framework_TestCase {
+
+    private static $wantedParams;
+
+    public function shunt_wantedParams()
+    {
+        $this->assertSame(self::$wantedParams, func_get_args(), 'wrong arguments were passed to the routine\'s callback');
+    }
+
+    protected function tearDown()
+    {
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
+        unset($_SERVER['HTTP_AUTHORIZATION']);
+    }
+
+    public function test_pass_all_params_to_callback()
+    {
+
+        $user = 'John';
+        $pass = 'Doe';
+        $param1 = 'abc';
+        $param2 = 'def';
+        self::$wantedParams = array($user, $pass, $param1, $param2);
+
+
+        $_SERVER['PHP_AUTH_USER'] = $user;
+        $_SERVER['PHP_AUTH_PW'] = $pass;
+        unset($_SERVER['HTTP_AUTHORIZATION']);
+
+        // to be able to run assertions in callback, I'm using a method here instead of a closure
+        $routine = new AuthBasic('auth realm', array($this, 'shunt_wantedParams'));
+        $routine->by(new \Respect\Rest\Request('GET', "/$param1/$param2"), array($param1, $param2));
+
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
+        $_SERVER['HTTP_AUTHORIZATION'] = 'Basic ' . base64_encode($user . ':' . $pass);
+
+        $routine = new AuthBasic('auth realm', array($this, 'shunt_wantedParams'));
+        $routine->by(new \Respect\Rest\Request('GET', "/$param1/$param2"), array($param1, $param2));
+    }
+}