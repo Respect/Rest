b0ccd893837494b19624b8462a4e807c2769b7bf
Alexandre Gaigalas <alexandre@gaigalas.net>
2012-08-27 06:05:48 -0300

    Adding class skeletons for Rel Routine.

---
+++ library/Respect/Rest/Routines/Rel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Respect\Rest\Routines;
+
+use Respect\Rest\Routes\AbstractSyncedRoutine;
+use Respect\Rest\Request;
+
+class Rel extends AbstractCallbackMediator implements ProxyableThrough
+{
+}
---
+++ tests/library/Respect/Rest/Routines/RelTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Respect\Rest\Routines;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use ReflectionFunction;
+
+/** 
+ * @covers Respect\Rest\Routines\Rel 
+ */
+class RelTest extends PHPUnit_Framework_TestCase
+{
+}
