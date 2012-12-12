69d18065e18e6fea34c1b1d8ff38bbe1b8e95205
Alexandre Gaigalas <alexandre@gaigalas.net>
2012-12-11 23:02:43 -0200

    Legibility improved for AbstractRoute

--- library/Respect/Rest/Routes/AbstractRoute.php
+++ library/Respect/Rest/Routes/AbstractRoute.php
@@ -37,141 +37,266 @@ use Respect\Rest\Routines\Unique;
  */
 abstract class AbstractRoute
 {
+    /** @const string Identifier for catch-all parameters in a route path */
     const CATCHALL_IDENTIFIER = '/**';
+    /** @const string Identifier for normal parameters in a route path */
     const PARAM_IDENTIFIER = '/*';
+    /** @const string Quoted version of the normal parameter identifier */
     const QUOTED_PARAM_IDENTIFIER = '/\*';
+    /** @const string A regular expression that cathes from a / to the end */
     const REGEX_CATCHALL = '(/.*)?';
+    /** @const string A regular expression that cathes one parameter */
     const REGEX_SINGLE_PARAM = '/([^/]+)';
+    /** @const string A regular expression that cathes one ending parameter */
     const REGEX_ENDING_PARAM = '#/\(\[\^/\]\+\)#';
+    /** @const string A regular expression that cathes one optional parameter */
     const REGEX_OPTIONAL_PARAM = '(?:/([^/]+))?';
+    /** @const string A regular expression that identifies invalid parameters */
     const REGEX_INVALID_OPTIONAL_PARAM = '#\(\?\:/\(\[\^/\]\+\)\)\?/#';
 
+    /** @var string The HTTP method for this route (GET, POST, ANY, etc) */
     public $method = '';
+    /** @var string The minimalistic pattern for route paths for this route */
     public $pattern = '';
+    /** @var string The generated regex for the route pattern */
     public $regexForMatch = '';
+    /** @var string The generated regex for creating URIs from parameters */
     public $regexForReplace = '';
+    /** @var array A list of routines appended to this route */
     public $routines = array();
+    /** @var array A list of side routes to be used */
     public $sideRoutes = array();
+    /** @var array A virtualhost applied to this route (deprecated) */
     public $virtualHost = null;
 
-    /** Returns the RelfectionFunctionAbstract object for the passed method */
+    /**
+     * Returns the RelfectionFunctionAbstract object for the passed method
+     *
+     * @param string $method The HTTP method (GET, POST, etc)
+     */
     abstract public function getReflection($method);
 
-    /** Runs the target method/params into this route */
+    /**
+     * Runs the target method/params into this route
+     *
+     * @param string $method The HTTP method (GET, POST, etc)
+     * @param array  $params A list of params to pass to the target
+     */
     abstract public function runTarget($method, &$params);
 
+    /**
+     * @param string $method  The HTTP method (GET, POST, etc)
+     * @param string $pattern The pattern for this route path
+     */
     public function __construct($method, $pattern)
     {
         $this->pattern = $pattern;
         $this->method = strtoupper($method);
+
         list($this->regexForMatch, $this->regexForReplace)
             = $this->createRegexPatterns($pattern);
     }
 
+    /**
+     * A magic routine builder and composite appender
+     *
+     * @param string $method    The HTTP method (GET, POST, etc)
+     * @param array  $arguments Arguments to pass to this routine constructor
+     * @see   Respect\Rest\Routes\AbstractRoute::appendRoutine
+     *
+     * @return AbstractRoute The route itselt
+     */
     public function __call($method, $arguments)
     {
-        $routineReflection = new ReflectionClass(
+        $reflection = new ReflectionClass(
                 'Respect\\Rest\\Routines\\' . ucfirst($method)
         );
 
-        return $this->appendRoutine($routineReflection->newInstanceArgs($arguments));
+        return $this->appendRoutine($reflection->newInstanceArgs($arguments));
     }
 
-    /** Appends a pre-built routine to this route */
+    /**
+     * Appends a pre-built routine to this route
+     *
+     * @param Routinable $routine A routine to be appended
+     * @see   Respect\Rest\Routes\AbstractRoute::__call
+     *
+     * @return AbstractRoute The route itselt
+     */
     public function appendRoutine(Routinable $routine)
     {
-        $key = $routine instanceof Unique ? get_class($routine) : spl_object_hash($routine);
+        $key = $routine instanceof Unique
+            ? get_class($routine)
+            : spl_object_hash($routine);
+
         $this->routines[$key] = $routine;
         return $this;
     }
 
-    /** Creates an URI for this route with the passed parameters */
+    /**
+     * Creates an URI for this route with the passed parameters, replacing
+     * them in the declared pattern. /hello/* with ['tom'] returns /hello/tom
+     *
+     * @param mixed $param1 Some parameter
+     * @param mixed $etc    This route accepts as many parameters you can pass
+     *
+     * @return string the created URI
+     */
     public function createUri($param1=null, $etc=null)
     {
         $params = func_get_args();
         array_unshift($params, $this->regexForReplace);
-        return rtrim($this->virtualHost, '/') . call_user_func_array(
-                'sprintf', $params
-        );
+
+        return rtrim($this->virtualHost, '/') .
+            call_user_func_array('sprintf', $params);
     }
 
+    /**
+     * Passes a request through all this routes ProxyableWhen routines
+     *
+     * @param Request $request The request you want to process
+     * @param array   $params  Parameters for the processed request
+     * @see   Respect\Rest\Routines\ProxyableWhen
+     *
+     * @return bool always true \,,/
+     */
     public function matchRoutines(Request $request, $params=array())
     {
-        foreach ($this->routines as $routine)
-            if ($routine instanceof ProxyableWhen
-                && !$request->routineCall('when', $request->method, $routine, $params))
+        foreach ($this->routines as $routine) {
+            if (
+                $routine instanceof ProxyableWhen
+                    && !$request->routineCall(
+                        'when',
+                        $request->method,
+                        $routine,
+                        $params)
+            ) {
                 return false;
+            }
+        }
 
         return true;
     }
 
-    /** Checks if this route matches a request */
+    /**
+     * Checks if a request passes for this route
+     *
+     * @param Request $request The request you want to process
+     * @param array   $params  Parameters for the processed request
+     *
+     * @return bool as true as xkcd (always true)
+     */
     public function match(Request $request, &$params=array())
     {
         $params = array();
         $matchUri = $request->uri;
 
-        foreach ($this->routines as $routine)
-            if ($routine instanceof IgnorableFileExtension)
-                $matchUri = preg_replace('#(\.[\w\d-_.~\+]+)*$#', '',
-                    $request->uri);
+        foreach ($this->routines as $routine) {
+            if ($routine instanceof IgnorableFileExtension) {
+                $matchUri = preg_replace(
+                    '#(\.[\w\d-_.~\+]+)*$#',
+                    '',
+                    $request->uri
+                );
+            }
+        }
 
-        if (!preg_match($this->regexForMatch, $matchUri, $params))
+        if (!preg_match($this->regexForMatch, $matchUri, $params)) {
             return false;
+        }
 
         array_shift($params);
 
-        if (false !== stripos($this->pattern, '/**') && false !== stripos(end($params), '/')) {
+        if (
+            false !== stripos($this->pattern, '/**')
+            && false !== stripos(end($params), '/')
+        ) {
             $lastParam = array_pop($params);
             $params[] = explode('/', ltrim($lastParam, '/'));
+        } elseif (
+            false !== stripos($this->pattern, '/**') && !isset($params[0])
+        ) {
+            $params[] = array(); // callback expects a parameter give it
         }
-        elseif (false !== stripos($this->pattern, '/**') && !isset($params[0]))
-                $params[] = array(); // callback expects a parameter give it
 
         return true;
     }
 
-    /** Creates the regex from the route patterns */
+    /**
+     * This creates a regular expression that matches a route pattern and
+     * extracts it's parameters
+     *
+     * @param string $pattern The pattern for the regex creation
+     *
+     * @return array A matcher regex and a replacer regex for createUri()
+     */
     protected function createRegexPatterns($pattern)
     {
         $pattern = rtrim($pattern, ' /');
         $extra = $this->extractCatchAllPattern($pattern);
         $matchPattern = str_replace(
-            static::QUOTED_PARAM_IDENTIFIER, static::REGEX_SINGLE_PARAM, preg_quote($pattern), $paramCount
+            static::QUOTED_PARAM_IDENTIFIER,
+            static::REGEX_SINGLE_PARAM,
+            preg_quote($pattern),
+            $paramCount
         );
-        $replacePattern = str_replace(static::PARAM_IDENTIFIER, '/%s', $pattern);
+        $replacePattern = str_replace(
+            static::PARAM_IDENTIFIER,
+            '/%s',
+            $pattern
+        );
         $matchPattern = $this->fixOptionalParams($matchPattern);
         $matchRegex = "#^{$matchPattern}{$extra}$#";
         return array($matchRegex, $replacePattern);
     }
 
-    /** Extracts the catch-all param from a pattern */
+    /**
+     * Extracts the catch-all parameter from a pattern and modifies the passed
+     * parameter to remove that. Yes, we're modifying by reference.
+     *
+     * @param string $pattern The pattern for the regex creation
+     *
+     * @return string The catch-all parameter or empty string
+     */
     protected function extractCatchAllPattern(&$pattern)
     {
         $extra = static::REGEX_CATCHALL;
 
-        if ((strlen($pattern) - strlen(static::CATCHALL_IDENTIFIER))
-            === strripos($pattern, static::CATCHALL_IDENTIFIER))
+        if (
+            (strlen($pattern) - strlen(static::CATCHALL_IDENTIFIER))
+                === strripos($pattern, static::CATCHALL_IDENTIFIER)
+        ) {
             $pattern = substr($pattern, 0, -3);
-        else
+        } else {
             $extra = '';
+        }
 
         $pattern = str_replace(
-            static::CATCHALL_IDENTIFIER, static::PARAM_IDENTIFIER, $pattern
+            static::CATCHALL_IDENTIFIER,
+            static::PARAM_IDENTIFIER,
+            $pattern
         );
         return $extra;
     }
 
-    /** Turn sequenced parameters optional */
+    /**
+     * Identifies using regular expressions a sequence of parameters in the end
+     * of a pattern and make the latest ones optional for the matcher regex
+     *
+     * @param string $quotedPattern a preg_quoted route pattern
+     */
     protected function fixOptionalParams($quotedPattern)
     {
-        if (strlen($quotedPattern) - strlen(static::REGEX_SINGLE_PARAM)
-            === strripos($quotedPattern, static::REGEX_SINGLE_PARAM))
+        if (
+            strlen($quotedPattern) - strlen(static::REGEX_SINGLE_PARAM)
+            === strripos($quotedPattern, static::REGEX_SINGLE_PARAM)
+        ) {
             $quotedPattern = preg_replace(
                 static::REGEX_ENDING_PARAM,
                 static::REGEX_OPTIONAL_PARAM,
                 $quotedPattern
             );
+        }
 
         $quotedPattern = preg_replace(
             static::REGEX_INVALID_OPTIONAL_PARAM,
