2ec8db5e016a254145e2e7fdbe7f827400021e99
Henrique Moody <henriquemoody@gmail.com>
2015-01-12 06:35:01 -0200

    Add license information on files' headers

--- library/Respect/Rest/Routines/AbstractCallbackMediator.php
+++ library/Respect/Rest/Routines/AbstractCallbackMediator.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * This file is part of the Respect\Rest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Respect\Rest\Routines;
 
 use Respect\Rest\Request;
